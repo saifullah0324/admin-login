22c6ea254514d08a232540d6f0a417c8d3ad88bd
saifullah0324 <syedsaif4113@gmail.com>
2024-11-28 12:01:18 +0500

    resolve merge

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -118,12 +118,9 @@
 							<a href="#" class="nav-link">
 								<i class="nav-icon fas fa-tag"></i>
 								<i class="fas fa-truck nav-icon"></i>
-								<p>Shopping</p>
+								<p>Shipping</p>
 							</a>
 						</li>
-
-						
-
 						<!-- <li class="nav-item">
 							<a href="orders.html" class="nav-link">
 								<i class="nav-icon fas fa-shopping-bag"></i>
